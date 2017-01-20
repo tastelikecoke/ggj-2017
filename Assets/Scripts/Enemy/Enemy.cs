7c19bdcdb9b9010ec52ab5c41c9815891bb24b87
Jeru Mercado <jmerc87@gmail.com>
2017-01-21 07:17:37 +0800

    Add sprite facing

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -17,6 +17,21 @@ public class Enemy : MonoBehaviour {
 	}
 	public void Update() {
 		transform.position += movement * Time.deltaTime;
+		Vector3 back = new Vector3(0f, 1f, 0f);
+		
+
+		if(movement.x >= 0) {
+			transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+		}
+		else {
+			transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
+		}
+
+		if(Mathf.Abs(Vector3.Angle(back, movement)) < 45f) {
+			Animator animator = GetComponent<Animator>();
+			animator.SetTrigger("Back");
+		}
+
 		if(transform.position.magnitude <= 1f) {
 			Explode();
 		}
