06576930263f1bcb3f1e81b9ce97583ebb078403
Jeru Mercado <jmerc87@gmail.com>
2017-01-21 06:15:58 +0800

    Make enemies of waves appear somehow

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	[System.NonSerialized]
+	public Vector3 movement;
+	[System.NonSerialized]
+	public int color;
+
+	public void Init(Vector3 movement, int color) {
+		this.movement = movement;
+		this.color = color;
+	}
+	public void Update() {
+		transform.position += movement * Time.deltaTime;
+		if(transform.position.magnitude <= 1f) {
+			Explode();
+		}
+	}
+	public void Explode() {
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyConfig.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public enum RaidType {
+	Enemy,
+	Delay,
+	Enemies,
+}
+
+[System.Serializable]
+public class Raid {
+	public RaidType type;
+	public int color;
+	public float time;
+}
+
+public class EnemyConfig : MonoBehaviour {
+	
+	private static EnemyConfig _instance;
+	
+	public List<Raid> raids;
+	public GameObject enemyPrefab;
+
+	void OnEnable() {
+		if (_instance == null) {
+			_instance = this;
+			DontDestroyOnLoad(this);
+		}
+	}
+	
+	static public EnemyConfig Instance {
+		get { return _instance; }
+	}
+
+	public void InitRaids() {
+		StartCoroutine(RaidCR());
+	}
+	public void Start() {
+		InitRaids();
+	}
+
+	public IEnumerator RaidCR() {
+		for(int idx = 0; idx < raids.Count; idx++) {
+			if(raids[idx].type == RaidType.Delay){
+				yield return new WaitForSeconds(raids[idx].time);
+			}
+			if(raids[idx].type == RaidType.Enemy){
+				GameObject enemy = Instantiate(enemyPrefab);
+				enemy.transform.SetParent(transform, true);
+				Vector2 randomPosition = Random.insideUnitCircle;
+
+				enemy.transform.position = (Vector3)randomPosition * 10f;
+				Enemy enemyScript = enemy.GetComponent<Enemy>();
+				enemyScript.Init(randomPosition * -2f, 0);
+			}
+		}
+		yield return null;
+	}
+}
