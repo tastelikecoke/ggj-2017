fb36200e74167f5d9ea612d89e479e0e118c3e4f
Joshua Castaneda <thejosh223@gmail.com>
2017-01-21 15:07:55 +0800

    Fixed the animatinos.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -26,10 +26,10 @@ public class Enemy : MonoBehaviour {
 		Vector3 back = new Vector3(0f, 1f, 0f);
 		whole.transform.localScale = new Vector3((movement.x >= 0 ? 1 : -1) * Mathf.Abs(whole.transform.localScale.x), whole.transform.localScale.y, whole.transform.localScale.z);
 
-		if(Mathf.Abs(Vector3.Angle(back, movement)) < 45f) {
-			Animator animator = GetComponent<Animator>();
-			animator.SetTrigger("Back");
-		}
+//		if(Mathf.Abs(Vector3.Angle(back, movement)) < 45f) {
+//			Animator animator = GetComponent<Animator>();
+//			animator.SetTrigger("Back");
+//		}
 
 		if (spriteCollider.enabled && transform.position.magnitude <= Tower.GetInstance().radius) {
 			OnGetToCenter();
