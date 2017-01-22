1685504d073c17fd26ec12e9fb706e555739b360
Joniel Ibasco <jlibasco@gmail.com>
2017-01-22 10:25:05 +0800

    Refactor tower stats/config

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using PubSub;
 
 public class Tower : MonoBehaviour {
 
@@ -11,13 +12,30 @@ public class Tower : MonoBehaviour {
 
 	int _lives = 10;
 	public event Action eventOnLivesChange = () => {};
-	public int lives { get { return _lives; } set { 
+
+	public int lives {
+		get { return _lives; }
+		set { 
 			if (_lives != value) {
 				_lives = value; 
 				eventOnLivesChange();
 				Debug.Log("On lives change");
 			}
-		} }
+		}
+	}
+
+	void OnEnable() {
+		GameMgr.Instance.PubSubMgr.Subscribe(Channel.GameStart, OnGameStart);
+	}
+
+	void OnDisable() {
+		GameMgr.Instance.PubSubMgr.Unsubscribe(Channel.GameStart, OnGameStart);
+	}
+
+	public void OnGameStart(Signal data) {
+		//reset stuff here
+		_lives = GameMgr.Instance.GameConfig.maxHP;
+	}
 
 	void Awake() {
 		__instance = this;
